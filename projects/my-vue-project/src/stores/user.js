9340445f3b2b1afbb809ca41a20db43307cfcd21
Josewer <josewer>
2025-08-13 20:48:29 +0200

    Se usa axios

--- projects/my-vue-project/src/stores/user.js
+++ projects/my-vue-project/src/stores/user.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { defineStore } from "pinia";
 import { ref } from "vue";
 
@@ -5,12 +6,23 @@ export const useUserStore = defineStore("user" , () => {
     
     const user = ref();
 
+/*
     const getUser = async () => {
         console.log("entro!")
         const URL_API = "https://jsonplaceholder.typicode.com/users/1";
         const response = await fetch(URL_API);
         const data = await response.json();
         user.value = data;
+        console.log(user.value.name)
+    }
+*/
+
+// Con AXIOS
+    const getUser = async () => {
+        console.log("entro!")
+        const URL_API = "https://jsonplaceholder.typicode.com/users/1";
+        const response = await axios.get(URL_API);
+        user.value = response.data;
         console.log(user.value.name)
     }
 
